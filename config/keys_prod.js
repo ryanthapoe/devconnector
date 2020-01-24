367e35723e6688b4cc8953531e35635a2182b5be
Ryantha Purba <45478201+ryanthapoe@users.noreply.github.com>
2020-01-24 16:15:20 +0700

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: 'mongodb+srv://ryantha:kBpYzaM0ZFeMp8dG@dbcluster-hk6b0.mongodb.net/test?retryWrites=true&w=majority',
   secretOrKey: process.env.SECRET_OR_KEY
 };
