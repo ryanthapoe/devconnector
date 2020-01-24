4034faf5836f9712741a62e93e435c4ca077c053
Ryantha Purba <45478201+ryanthapoe@users.noreply.github.com>
2020-01-24 16:09:44 +0700

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://ryantha:ryantha@cluster0-xdbfv.mongodb.net/test?retryWrites=true&w=majority',
+    'mongodb+srv://ryantha:123abc@dbcluster-hk6b0.mongodb.net/test?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
+
